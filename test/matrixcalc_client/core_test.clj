cbe0ca106cfaff0ad5164338921cc102462027e2
Matus Laslofi <matus.laslofi@gmail.com>
2016-04-09 12:30:01 +0200

    Forgot to save

--- test/matrixcalc_client/core_test.clj
+++ test/matrixcalc_client/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [matrixcalc-client.core :refer :all]))
 
+(def url
+  "http://matrixcalc.demecko.com/api/add/1-1/2-2")
+
+(def mtx
+  {:matrix [[1 2 3]
+            [4 5 6]
+            [7 8 9]]})
+
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
