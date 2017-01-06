8cfbc1bda67b031f659f69b3169b28e2c4e063bb
Matus Laslofi <matus.laslofi@gmail.com>
2017-01-06 11:35:48 +0100

    Remove unused files

--- test/matrixcalc_client/core_test.clj
+++
@@ -1,8 +0,0 @@
-
-(ns matrixcalc-client.core-test
-  (:require [clojure.test :refer :all]
-            [matrixcalc-client.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
